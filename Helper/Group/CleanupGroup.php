06efcb5ea1c14894d596989c03c11fd0ee3b7e46
Erik Perri <erik.perri@gmail.com>
2019-01-17 22:22:45 -0500

    Cleanup

--- Helper/Group/CleanupGroup.php
+++ Helper/Group/CleanupGroup.php
@@ -5,6 +5,10 @@ namespace Ryvon\EventLog\Helper\Group;
 class CleanupGroup extends AbstractGroup
 {
     /**
+     * You should not use this in any plugins interacting with the event log.
+     * They should use the string so they do not fail when the event log in not
+     * installed.
+     *
      * @var string
      */
     const GROUP_ID = 'cleanup';
