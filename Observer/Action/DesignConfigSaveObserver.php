c191dba518f83a24e86e11e912a6ab560a813a49
Erik Perri <erik.perri@gmail.com>
2019-07-13 18:20:31 -0400

    Added design configuration to log

---
+++ Observer/Action/DesignConfigSaveObserver.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ryvon\EventLog\Observer\Action;
+
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\Event\ManagerInterface;
+
+/**
+ * Monitors for the design config save request.
+ */
+class DesignConfigSaveObserver implements ActionObserverInterface
+{
+    /**
+     * @var ManagerInterface
+     */
+    private $eventManager;
+
+    /**
+     * @param ManagerInterface $eventManager
+     */
+    public function __construct(ManagerInterface $eventManager)
+    {
+        $this->eventManager = $eventManager;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(Http $request)
+    {
+        $scope = $request->get('scope');
+        $scopeId = $request->get('scope_id');
+
+        $this->eventManager->dispatch('event_log_info', [
+            'group' => 'admin',
+            'message' => 'Design configuration scope {design-config-scope} modified.',
+            'context' => [
+                'design-config-scope' => $scope,
+                'design-config-scope-id' => $scopeId,
+            ],
+        ]);
+    }
+}
