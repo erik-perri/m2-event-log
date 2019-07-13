d434795092ccfdb340ff7c9d8d908b3b096fd7b7
Erik Perri <erik.perri@gmail.com>
2019-07-13 18:26:56 -0400

    Added design config scope placeholder

---
+++ Helper/Placeholder/DesignConfigScopePlaceholder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Ryvon\EventLog\Helper\Placeholder;
+
+use Magento\Backend\Model\UrlInterface;
+use Magento\Framework\DataObject;
+
+class DesignConfigScopePlaceholder implements PlaceholderInterface
+{
+    use LinkPlaceholderTrait;
+
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+
+    /**
+     * @param UrlInterface $urlBuilder
+     */
+    public function __construct(UrlInterface $urlBuilder)
+    {
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSearchString(): string
+    {
+        return 'design-config-scope';
+    }
+
+    /**
+     * @param DataObject $context
+     * @return string|null
+     */
+    public function getReplaceString($context)
+    {
+        $scope = $context->getData('design-config-scope');
+        if (!$scope) {
+            return null;
+        }
+
+        $params = [
+            'scope' => $scope,
+        ];
+
+        $scopeId = $context->getData('design-config-scope-id');
+        if ($scopeId) {
+            $params['scope_id'] = $scopeId;
+        }
+
+        return $this->buildLinkTag([
+            'text' => $scope,
+            'title' => 'Edit this configuration scope in the admin',
+            'href' => $this->urlBuilder->getUrl('theme/design_config/edit', $params),
+            'target' => '_blank',
+        ]);
+    }
+}
