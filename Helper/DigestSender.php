fc12c3f4919e9ff2c5eb5374b0b71a8303a59f8b
Erik Perri <erik.perri@gmail.com>
2019-07-16 06:17:59 -0400

    Added ability for event listener to cancel the email sending

--- Helper/DigestSender.php
+++ Helper/DigestSender.php
@@ -2,12 +2,16 @@
 
 namespace Ryvon\EventLog\Helper;
 
-use Ryvon\EventLog\Model\Digest;
-use Ryvon\EventLog\Model\DigestRepository;
+use Magento\Framework\DataObjectFactory;
 use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Exception\MailException;
 use Psr\Log\LoggerInterface;
+use Ryvon\EventLog\Model\Digest;
+use Ryvon\EventLog\Model\DigestRepository;
 
+/**
+ * Helper class to finish and send the digest.
+ */
 class DigestSender
 {
     /**
@@ -19,6 +23,11 @@ class DigestSender
      * @var ManagerInterface
      */
     private $eventManager;
+
+    /**
+     * @var DataObjectFactory
+     */
+    private $dataObjectFactory;
 
     /**
      * @var EmailBuilder
@@ -33,22 +42,27 @@ class DigestSender
     /**
      * @param LoggerInterface $logger
      * @param ManagerInterface $eventManager
+     * @param DataObjectFactory $dataObjectFactory
      * @param DigestRepository $digestRepository
      * @param EmailBuilder $emailHelper
      */
     public function __construct(
         LoggerInterface $logger,
         ManagerInterface $eventManager,
+        DataObjectFactory $dataObjectFactory,
         DigestRepository $digestRepository,
         EmailBuilder $emailHelper
     ) {
         $this->eventManager = $eventManager;
         $this->digestRepository = $digestRepository;
+        $this->dataObjectFactory = $dataObjectFactory;
         $this->emailHelper = $emailHelper;
         $this->logger = $logger;
     }
 
     /**
+     * Finish the digest and create a new one.
+     *
      * @param Digest $digest
      * @return bool
      */
@@ -75,6 +89,8 @@ class DigestSender
     }
 
     /**
+     * Send the digest email.
+     *
      * @param Digest $digest
      * @return bool
      */
@@ -82,11 +98,18 @@ class DigestSender
     {
         $builder = $this->emailHelper->createDigestEmail($digest);
 
-        $this->eventManager->dispatch('event_log_email_sending', [
-            'instance' => $this,
+        $eventData = $this->dataObjectFactory->create([
+            'continue' => true,
             'digest' => $digest,
             'builder' => $builder,
         ]);
+
+        $this->eventManager->dispatch('event_log_email_sending', ['data' => $eventData]);
+
+        if (!$eventData->getData('continue')) {
+            $this->logger->info('Cancelling email send due to "event_log_email_sending" event request.');
+            return true;
+        }
 
         try {
             $builder->getTransport()->sendMessage();
